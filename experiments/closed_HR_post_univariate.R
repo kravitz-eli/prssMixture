aa3ff3a12a16bef7db4b9a6aab4eb56da0e9fb66
Eli Kravitz <kravitz_eli@lilly.com>
2020-01-30 09:16:59 -0500

    run ESS simulations

--- experiments/closed_HR_post_univariate.R
+++ experiments/closed_HR_post_univariate.R
@@ -1,17 +1,3 @@
-# # Scratch work to confirm I'm calculate conjugates correctly
-# prior_mean = 0
-# n_P = 32
-# prior_var = 4 / n_P
-#
-# # Data values
-# L_mean = log(2.25)
-# n_L = 45
-# L_var = 4/45
-#
-# # Get posterior mean and var with conjugacy
-# post_var = 4 / (n_L + n_P)
-# post_sd = sqrt(post_var)
-# post_mean = (n_L * L_mean + n_P * prior_mean) / (n_L + n_P)
 
 closed_HR_post = function(prior_N, prior_mean, data_N, data_mean){
 
